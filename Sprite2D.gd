412da0d67923cc04c900b065512292e53e6b0793
Paul Winkler <slinkp@gmail.com>
2023-07-14 12:24:03 -0400

    "Listening to Player Input": movement and rotation controlled by keys

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -7,8 +7,16 @@ func _init():
 	print("Hello, world!")
 
 func _process(delta):
-	rotation += angular_speed * delta
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
 	
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	rotation += angular_speed * direction * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+	
 	position += velocity * delta
-	
