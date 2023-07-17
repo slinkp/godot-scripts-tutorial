c90221e88b04fda8ba8936fd1bb59f852b2370c8
Paul Winkler <slinkp@gmail.com>
2023-07-17 15:51:26 -0700

    Playing with connecting custom signals and changing state in handlers

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -2,7 +2,8 @@ extends Sprite2D
 
 var speed = 400
 var angular_speed = PI
-var health = 100
+var initial_health = 100
+var health = initial_health
 signal health_depleted  # Defines a custom signal as per https://docs.godotengine.org/en/stable/getting_started/step_by_step/signals.html#custom-signals
 signal health_changed(new_value)
 
@@ -13,7 +14,8 @@ func _ready():
 	print("Hello, ready!")
 	var timer = get_node("Paul's Cool Timer")
 	timer.timeout.connect(_on_paul_timer_timeout)
-	
+	health_depleted.connect(_on_health_depleted)
+	health_changed.connect(_on_health_changed)
 
 func _process(delta):
 	rotation += angular_speed * delta
@@ -22,7 +24,12 @@ func _process(delta):
 
 func _on_button_pressed():
 	print("Hello button")
+	reset_health()
 	set_process(not is_processing())
+
+func reset_health():
+	health = initial_health
+	print("Health was reset to %s" % health)
 
 func _on_paul_timer_timeout():
 	# Every time the timer cycles, we flip sprite visibility on/off
@@ -32,3 +39,9 @@ func _on_paul_timer_timeout():
 	if health < 10 and health >= 0:
 		print("Emitting custom signal")
 		health_depleted.emit()
+
+func _on_health_depleted():
+	print("Received health_depleted event")
+
+func _on_health_changed(amount):
+	print("Received health_changed with amount %s" % amount)
