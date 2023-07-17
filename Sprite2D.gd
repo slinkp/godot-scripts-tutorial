cd46c1af7657c0c19a33a5c110c0ff337198dfa8
Paul Winkler <slinkp@gmail.com>
2023-07-17 15:38:45 -0700

    Connecting a Signal via Code done:Toggle visibility on a timer that cycles

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -6,6 +6,12 @@ var angular_speed = PI
 func _init():
 	print("Hello, world!")
 
+func _ready():
+	print("Hello, ready!")
+	var timer = get_node("Paul's Cool Timer")
+	timer.timeout.connect(_on_paul_timer_timeout)
+	
+
 func _process(delta):
 	rotation += angular_speed * delta
 	var velocity = Vector2.UP.rotated(rotation) * speed
@@ -14,3 +20,7 @@ func _process(delta):
 func _on_button_pressed():
 	print("Hello button")
 	set_process(not is_processing())
+
+func _on_paul_timer_timeout():
+	# Every time the timer cycles, we flip sprite visibility on/off
+	visible = not visible
